c858200620d8763959062026c9a6c2e31bc56bf2
Krizshel Benaly Cabral <krizbencabral@gmail.com>
2021-05-27 15:49:16 +0800

    Adding data in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
       const { current } = body;
       callback(
         undefined,
-        `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`
+        `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out and the humidity is ${current.humidity}.`
       );
     }
   });
